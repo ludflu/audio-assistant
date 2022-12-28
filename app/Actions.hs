6507cec3011775e55fc52e56cf9085471c792210
Jim Snavely <ludflu@gmail.com>
2022-12-27 20:58:27 -0500

    better time and date

--- app/Actions.hs
+++ app/Actions.hs
@@ -16,16 +16,51 @@ import Data.Time.LocalTime
 import Data.Char ( isSpace, isUpper, toUpper )
 
 
+import Data.Time                             -- package "time"
+import Data.Time.Calendar.WeekDate           -- package "time"
+import Data.Time.LocalTime.TimeZone.Olson    -- package "timezone-olson"
+import Data.Time.LocalTime.TimeZone.Series
+
 command :: FilePath
 command = "/home/jsnavely/project/audio-take-two/player/talk.sh"
 
+getLocalTime = do time <- getCurrentTime
+                  zone <- getCurrentTimeZone
+                  return $ utcToLocalTime zone time
+
+
+monthMap :: M.Map Int String 
+monthMap = M.fromList [ 
+                        (1,"January"), 
+                        (2,"February"), 
+                        (3,"March"),
+                        (4,"April"),
+                        (5,"May"),
+                        (6,"June"),
+                        (7,"July"),
+                        (8,"August"),
+                        (9,"September"),
+                        (10,"October"),
+                        (11,"November"),
+                        (12,"December")
+                     ]
+
+getMonthString :: Int -> String
+getMonthString month = let mstr = M.lookup month monthMap
+                        in fromMaybe "" mstr
+
+currentDay :: IO String
+currentDay = do localTime <- getLocalTime
+                let LocalTime day (TimeOfDay hh mm ss) = localTime
+                    (yr, mn, dom) = toGregorian day
+                    (_,  wk, dow) = toWeekDate day
+                    formatDay :: String =  getMonthString mn ++ " " ++ show dom ++ ", " ++ show yr 
+                return formatDay
+
 currentTime :: IO String
-currentTime = do time <- getCurrentTime
-                 zone <- getCurrentTimeZone
-                 let tt = show time
-                     localTime = utcToLocalTime zone time
-                     timeOfDay = localTimeOfDay localTime
-                     hour = show $ todHour timeOfDay
+currentTime = do localTime <- getLocalTime
+                 let timeOfDay = localTimeOfDay localTime
+                     hour = show $ (todHour timeOfDay) `mod` 12
                      minutes = show $ todMin timeOfDay
                      theTime = hour ++ " " ++ minutes
                  return theTime
@@ -35,7 +70,8 @@ responses :: M.Map String (IO String)
 responses = M.fromList [ 
     ("hello computer", return "Hello Jim"),
     ("peace be with you", return "And also with you"),
-    ("computer what time is it",  currentTime)
+    ("computer what time is it",  currentTime),
+    ("computer what day is it",  currentDay)
                        ]
 capitalise :: [Char] -> [Char]
 capitalise = map toUpper
