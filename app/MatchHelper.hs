4fd8de576a55393d02df32050c8add191e8ed4ef
Jim Snavely <ludflu@gmail.com>
2023-07-22 18:32:27 -0400

    better nums

--- app/MatchHelper.hs
+++ app/MatchHelper.hs
@@ -2,7 +2,9 @@
 
 module MatchHelper where
 
+import Control.Applicative ((<|>))
 import Data.Char (isNumber, toLower)
+import qualified Data.Map as M
 import Text.Read (readMaybe)
 import Text.Regex.PCRE.Heavy (Regex, re, scan)
 
@@ -22,5 +24,33 @@ isMatch s r = not (null (scan r s))
 dropNonNumbers :: String -> String
 dropNonNumbers = filter isNumber
 
+nums :: [String]
+nums = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", "twenty"]
+
+teens :: [String]
+teens = ["", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety", "hundred"]
+
+numsToText :: Int -> String
+numsToText i
+  | i <= 20 = nums !! i
+  | i < 100 = teens !! ((i `div` 10) - 1) ++ " " ++ nums !! (i `mod` 10)
+
+numsToText' :: Integer -> String
+numsToText' i =
+  let ii = fromInteger i
+   in numsToText ii
+
+nummap :: M.Map String Integer
+nummap =
+  let numnum = map toInteger [1 .. 100]
+      numstrs = map numsToText' numnum
+   in M.fromList (zip numstrs numnum)
+
+lookupNum :: String -> Maybe Integer
+lookupNum i = M.lookup i nummap
+
 parseInt :: String -> Maybe Integer
 parseInt str = readMaybe $ dropNonNumbers str
+
+readInt :: String -> Maybe Integer
+readInt str = parseInt str <|> lookupNum str
