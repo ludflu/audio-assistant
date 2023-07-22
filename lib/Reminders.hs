bf5d0d054afd1a644a21d1bc9ea984137972272f
Jim Snavely <ludflu@gmail.com>
2023-07-22 11:49:37 -0400

    reminder

--- lib/Reminders.hs
+++ lib/Reminders.hs
@@ -14,11 +14,24 @@ import Control.Monad.State
 import Listener (ListenerMonad, ListenerState (mailbox), listenPatiently, say)
 import MatchHelper (parseInt)
 
-sendReminder :: Integer -> String -> MVar String -> IO ()
-sendReminder seconds reminder mailbox = do
+sendReminder' :: Integer -> String -> MVar String -> IO ()
+sendReminder' seconds reminder mailbox = do
   threadDelay (10 ^ 6 * fromInteger seconds)
   putMVar mailbox reminder
   return ()
+
+sendReminder :: Integer -> String -> MVar String -> IO ()
+sendReminder seconds reminder mailbox = do
+  forkIO $ sendReminder' seconds reminder mailbox
+  return ()
+
+setReminder' :: Integer -> ListenerMonad ()
+setReminder' seconds = do
+  listener <- get
+  say "what's the reminder?"
+  reminder <- listenPatiently
+  let box = mailbox listener
+  liftIO $ sendReminder seconds reminder box
 
 setReminder :: [String] -> ListenerMonad String
 setReminder s = do
@@ -28,11 +41,3 @@ setReminder s = do
    in case seconds of
         Just secs -> setReminder' secs >> return "reminder set."
         Nothing -> return "Invalid time"
-
-setReminder' :: Integer -> ListenerMonad ()
-setReminder' seconds = do
-  listener <- get
-  say "what's the reminder?"
-  reminder <- listenPatiently
-  let box = mailbox listener
-  liftIO $ sendReminder seconds reminder box
