ae1e4f351d36f66529472453954219f5d0c2159d
Jim Snavely <ludflu@gmail.com>
2023-11-21 10:08:18 -0500

    remove system prompt

--- lib/OllamaApi.hs
+++ lib/OllamaApi.hs
@@ -72,7 +72,7 @@ getAnswer jr =
 
 answerQuestion :: String -> IO String
 answerQuestion question = runReq defaultHttpConfig $ do
-  let payload = OllamaRequest {model = "llama2", prompt = "In one sentence: " ++ question, stream = False}
+  let payload = OllamaRequest {model = "llama2", prompt = question, stream = False}
   let reqBody = ReqBodyJson payload
   let url = "127.0.0.1"
   let apiPort = 11434
