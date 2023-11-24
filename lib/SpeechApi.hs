280afd7a1ec3138612e4e051df618dc370a0e437
Jim Snavely <ludflu@gmail.com>
2023-11-24 14:14:12 -0500

    Speech api (#7)
    * use the speech api
    
    * fix field
    
    * switch to http conduit
    
    * try to fix
    
    * try imports
    
    * header
    
    * simplify
    
    * try setting all the stuff
    
    * try either
    
    * duration

--- lib/SpeechApi.hs
+++ lib/SpeechApi.hs
@@ -1,62 +1,87 @@
+{-# LANGUAGE BlockArguments #-}
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module SpeechApi (sayText) where
 
-import Control.Monad.IO.Class ()
-import Data.Aeson (FromJSON, ToJSON, Value (Number, Object, String), fromJSON, parseJSON)
+import Conduit (ConduitM, ConduitT, MonadResource, awaitForever, concatC, concatMapAccumC, concatMapC, concatMapCE, filterC, leftover, mapAccumWhileC, mapC, mapCE, mapM_C, runConduit, runConduitRes, sinkLazy, sourceLazy, yield, (.|))
+import Control.Concurrent (forkIO)
+import Control.Concurrent.STM (STM, TQueue, atomically, readTVar, writeTQueue, writeTVar)
+import Control.Exception (throwIO)
+import Control.Monad (liftM, unless, when)
+import Control.Monad.IO.Class (MonadIO (..))
+import Control.Monad.Trans.Resource (ResourceT, liftResourceT, runResourceT)
+import Data.Aeson (FromJSON, ToJSON, Value (Number, Object, String), decode, eitherDecode, encode, fromJSON, json, parseJSON)
+import Data.Aeson.Encoding (string)
 import qualified Data.Aeson.KeyMap as AKM
-import Data.Scientific (toRealFloat)
-import Data.Text (Text, unpack)
+import qualified Data.ByteString as B
+import Data.ByteString.Builder (byteString)
+import Data.ByteString.Char8 (unpack)
+import qualified Data.ByteString.Lazy as BLS
+import Data.Char (isPunctuation)
+import Data.Conduit.Binary (sinkFile, sinkHandle, sinkLbs, sourceLbs)
+import Data.Conduit.Combinators (concatMapE, concatMapM, mapAccumWhile, mapE, splitOnUnboundedE)
+import Data.IntMap.Merge.Lazy (mapWhenMatched)
+import Data.List (isInfixOf)
+import Data.Maybe (fromJust, isJust, mapMaybe)
+import qualified Data.Text as T
+import Data.Text.Array (run)
+import qualified Data.Text.Encoding as TE
+import Data.Word (Word8)
 import GHC.Generics (Generic)
-import qualified Network.HTTP.Client.MultipartFormData as LM
-import Network.HTTP.Req
-  ( JsonResponse,
-    POST (POST),
-    ReqBodyJson (ReqBodyJson),
-    defaultHttpConfig,
+import Network.HTTP.Conduit
+  ( Request (method, port, requestBody, requestHeaders, responseTimeout, secure),
+    RequestBody (RequestBodyBS, RequestBodyLBS),
+    Response (responseBody),
     http,
-    jsonResponse,
-    port,
-    req,
-    reqBodyMultipart,
-    responseBody,
-    runReq,
-    (/:)
+    httpLbs,
+    newManager,
+    parseRequest,
+    responseTimeoutMicro,
+    tlsManagerSettings,
   )
+import Network.HTTP.Req (req)
+import Network.HTTP.Simple (getResponseBody, httpJSON, httpLBS, setRequestBodyJSON, setRequestHeaders, setRequestMethod, setRequestPort, setRequestResponseTimeout)
+import Network.HTTP.Types
 
 newtype SpeechRequest = SpeechRequest
   { message :: String
   }
-  deriving (Generic)
+  deriving (Generic, Show)
 
-newtype SpeechResponse = SpeechResponse
-  { duration :: Double
+data SpeechResponse = SpeechResponse
+  { duration :: Double,
+    status :: String
   }
-  deriving (Generic)
+  deriving (Generic, Show)
 
 instance ToJSON SpeechRequest
 
 instance FromJSON SpeechResponse
 
-getDuration :: JsonResponse SpeechResponse -> Double
-getDuration jr =
-  let sr = responseBody jr
-   in duration sr
+parseDuration :: BLS.ByteString -> Maybe Double
+parseDuration rsp =
+  let srsp = decode rsp
+   in fmap duration srsp
 
 sayText :: String -> IO Double
-sayText message = runReq defaultHttpConfig $ do
-  let payload = SpeechRequest message
-  let reqBody = ReqBodyJson payload
-  let url = "127.0.0.1"
+sayText msg =
+  let payload = SpeechRequest {message = msg}
+      url = "http://127.0.0.1/talk"
+      apiPort = 5002
+   in do
+        request' <- parseRequest url
+        let request =
+              setRequestResponseTimeout (responseTimeoutMicro (500 * 1000000))
+                . setRequestHeaders [(hContentType, "application/json")]
+                . setRequestBodyJSON payload
+                . setRequestMethod "POST"
+                . setRequestPort apiPort
+                $ request'
 
-  r <-
-    req
-      POST
-      (http url /: "talk")
-      reqBody
-      jsonResponse
-      $ port 5001
-  return $
-    getDuration r
+        rsp <- httpLBS request
+        liftIO $ print $ getResponseBody rsp
+        return $ fromJust $ parseDuration $ getResponseBody rsp
