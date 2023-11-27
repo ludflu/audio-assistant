b8516799c1bf82ca5adc1822d415eb7c86a1cbe3
Jim Snavely <ludflu@gmail.com>
2023-11-26 20:35:53 -0500

    break out the chunker

--- lib/OllamaApi.hs
+++ lib/OllamaApi.hs
@@ -104,6 +104,15 @@ answerQuestion url port mailbox question = do
   forkIO $ answerQuestion' url port mailbox question
   return ()
 
+chunker :: Monad m => (String -> m ()) -> ConduitT B.ByteString c m ()
+chunker chunkAction =
+  jsonChunks '}'
+    .| mapC makeResponseChunk
+    .| filterC isJust
+    .| mapC (getAnswer . fromJust)
+    .| splitOnUnboundedE isPunct
+    .| mapM_C chunkAction
+
 answerQuestion' :: String -> Int -> TQueue String -> String -> IO ()
 answerQuestion' url apiPort mailbox question =
   let payload = OllamaRequest {model = "llama2", prompt = question, stream = True}
@@ -112,15 +121,10 @@ answerQuestion' url apiPort mailbox question =
         request' <- parseRequest url
         let request'' = request' {method = "POST", requestBody = body, port = apiPort}
             request = setRequestResponseTimeout (responseTimeoutMicro (500 * 1000000)) request''
+            chunkAction = writeToMailBox' mailbox
         manager <- newManager tlsManagerSettings
         runResourceT $
           do
             rsp <- http request manager
             runConduit $
-              responseBody rsp
-                .| jsonChunks '}'
-                .| mapC makeResponseChunk
-                .| filterC isJust
-                .| mapC (getAnswer . fromJust)
-                .| splitOnUnboundedE isPunct
-                .| mapM_C (writeToMailBox' mailbox)
+              responseBody rsp .| chunker chunkAction
